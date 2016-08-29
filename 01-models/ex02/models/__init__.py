c19321c78cc734f218c863d3900f830b1ca9567c
Stan <sts@odoo.com>
2016-08-29 14:15:17 +0200

    [ADD] module 01: models and fields

---
+++ 01-models/ex02/models/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+# import filename_python_file_within_folder_or_subfolder
---
+++ 01-models/ex03/models/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+# import filename_python_file_within_folder_or_subfolder
+import planning
