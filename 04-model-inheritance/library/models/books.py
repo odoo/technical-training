b10fd7f1fb3a4cbd542777d613b759e46d0c5e73
Raphael Collet <rco@openerp.com>
2016-09-21 16:55:47 +0200

    [ADD] 04-model-inheritance: add solutions

--- 04-model-inheritance/library/models/books.py
+++ 04-model-inheritance/library/models/books.py
@@ -10,4 +10,15 @@ class Books(models.Model):
     edition_date =  fields.Date(string='Edition date',)
     isbn = fields.Char(string='ISBN')
     publisher_id = fields.Many2one('library.publisher', string='Publisher')
-    rental_ids = fields.One2many('library.rental', 'book_id', string='Rentals')
+    copy_ids = fields.One2many('library.copy', 'book_id', string="Book Copies")
+
+
+class BookCopy(models.Model):
+    _name = 'library.copy'
+    _description = 'Book Copy'
+    _rec_name = 'reference'
+
+    book_id = fields.Many2one('library.book', string="Book"
+                              required=True, ondelete="cascade", delegate=True)
+    reference = fields.Char(string="Reference")
+    rental_ids = fields.One2many('library.rental', 'copy_id', string='Rentals')
