14e7b58fbf2ccbbc2c95437216e03dbf9e5f3137
Raphael Collet <rco@openerp.com>
2016-09-23 15:27:02 +0200

    [FIX] 05-view-inheritance: delegate field should be required

--- 05-view-inheritance/ex02/models/books.py
+++ 05-view-inheritance/ex02/models/books.py
@@ -18,6 +18,7 @@ class BookCopy(models.Model):
     _description = 'Book Copy'
     _rec_name = 'reference'
 
-    book_id = fields.Many2one('library.book', string="Book", delegate=True)
+    book_id = fields.Many2one('library.book', string="Book",
+                              required=True, ondelete="cascade", delegate=True)
     reference = fields.Char(string="Reference")
     rental_ids = fields.One2many('library.rental', 'copy_id', string='Rentals')
