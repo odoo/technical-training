c70eaa00a9812cd13d5722a7fec59982b0753658
sts-odoo <sts@odoo.com>
2017-08-18 12:48:53 +0200

    [IMP] simplified starting point of academy on 10

--- 10-reports/openacademy/__manifest__.py
+++ 10-reports/openacademy/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'board', 'website', 'website_sale'],
+    'depends': ['base', 'mail','board',],
 
     # always loaded
     'data': [
@@ -33,9 +33,7 @@
         'views/courses_kanban.xml',
         'wizard/add_attendee_view.xml',
         'report/board.xml',
-        'data/session_workflow.xml',
         'data/partner.xml',
-        'templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
