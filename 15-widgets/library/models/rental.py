775e395cddde311c6dbd76455a144253d34fe4c3
Yenthe V.G <yenthevg@gmail.com>
2017-11-08 16:04:11 +0100

    [FIX] 15-widgets: Set title on records

--- 15-widgets/library/models/rental.py
+++ 15-widgets/library/models/rental.py
@@ -5,6 +5,7 @@ from datetime import datetime
 class Rental(models.Model):
     _name = 'library.rental'
     _description = 'Book rental'
+    _rec_name = 'book_id'
     _order = "rental_date desc,return_date desc"
 
     customer_id = fields.Many2one(
