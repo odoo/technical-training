ecf2e32e4236ece65d7586f938216bf7d75f2f02
Stanislas Sobieski <sts@odoo.com>
2018-09-10 16:02:21 +0200

    [SOLUTION] 11 - Controller

--- library/__manifest__.py
+++ library/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'product'],
+    'depends': ['base', 'product', 'website', 'website_sale'],
 
     # always loaded
     'data': [
