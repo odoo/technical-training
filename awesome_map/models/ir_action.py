9e2764c2d9b3344c4e66545deb4de1b12d65d9be
Stanislas Sobieski <sts@odoo.com>
2018-09-10 14:05:10 +0200

    [DEV] 17 - Creating Views

---
+++ awesome_map/models/ir_action.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, models
+
+
+
+class ActWindowView(models.Model):
+    _inherit = 'ir.actions.act_window.view'
+
+    view_mode = fields.Selection(selection_add=[('map', "Map")])
