bee08e96dfc845b7d390ea238a204cbcac5a93ea
Nicolas Martinelli <nim@odoo.com>
2018-09-10 13:56:35 +0200

    Add link to Technical Training for Odoo 16.0

---
+++ estate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Estate",  # The name that will appear in the App list
+    "version": "16.0.0",  # Version
+    "application": True,  # This line says the module is an App, and not a module
+    "depends": ["base"],  # dependencies
+    "data": [
+
+    ],
+    "installable": True,
+    'license': 'LGPL-3',
+}
