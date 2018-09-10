01551288d7b3ef4a9c0ae1a9df89c69dea8a409b
Stanislas Sobieski <sts@odoo.com>
2018-09-10 15:27:38 +0200

    [SOLUTION] 02 - Fields

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import fields, models
+from odoo import api, exceptions, fields, models
 
 
 class Course(models.Model):
@@ -29,3 +29,45 @@ class Session(models.Model):
     instructor_id = fields.Many2one('openacademy.partner', string="Instructor")
     course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('openacademy.partner', string="Attendees")
+    seats = fields.Integer()
+
+    ###
+    # Using computed fields
+    ###
+    taken_seats = fields.Float(compute='_compute_taken_seats', store=True)
+
+    @api.depends('seats', 'attendee_ids')
+    def _compute_taken_seats(self):
+        for session in self:
+            if not session.seats:
+                session.taken_seats = 0.0
+            else:
+                session.taken_seats = 100.0 * len(session.attendee_ids) / session.seats
+
+    ###
+    # using onchange
+    ###
+    @api.onchange('seats', 'attendee_ids')
+    def _change_taken_seats(self):
+        if self.taken_seats > 100:
+            return {'warning': {
+                'title':   'Too many attendees',
+                'message': 'The room has %s available seats and there is %s attendees registered' % (self.seats, len(self.attendee_ids))
+            }}
+
+    ###
+    # using python constrains
+    ###
+    @api.constrains('seats', 'attendee_ids')
+    def _check_taken_seats(self):
+        for session in self:
+            if session.taken_seats > 100:
+                raise exceptions.ValidationError('The room has %s available seats and there is %s attendees registered' % (session.seats, len(session.attendee_ids)))
+
+    ###
+    # using SQL constrains
+    ###
+    _sql_constraints = [
+        # possible only if taken_seats is stored
+        ('session_full', 'CHECK(taken_seats <= 100)', 'The room is full'),
+    ]
