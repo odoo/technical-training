80bcf5db651482c97fee5c04b4b8be124cb7c7f7
Aaron Bohy <aab@odoo.com>
2018-09-28 14:48:01 +0200

    solution Task 3.2

--- awesome_map/static/src/js/map_model.js
+++ awesome_map/static/src/js/map_model.js
@@ -3,7 +3,54 @@ odoo.define('awesome_map.MapModel', function (require) {
 
 const AbstractModel = require('web.AbstractModel');
 
-const MapModel = AbstractModel.extend({});
+const MapModel = AbstractModel.extend({
+    /**
+     * @override
+     */
+    init: function () {
+        this._super.apply(this, arguments);
+        this.data = null;
+    },
+
+    //--------------------------------------------------------------------------
+    // Public
+    //--------------------------------------------------------------------------
+
+    /**
+     * @override
+     * @returns {Object}
+     */
+    get: function () {
+        return this.data;
+    },
+    /**
+     * @override
+     * @param {string} params.latitudeField
+     * @param {string} params.longitudeField
+     * @param {string} params.modelName
+     * @param {Object} [params.context]
+     * @param {Array[]} [params.domain]
+     * @returns {Promise}
+     */
+    load: function (params) {
+        const fields = [params.latitudeField, params.longitudeField];
+        return this._rpc({
+            model: params.modelName,
+            method: 'search_read',
+            context: params.context || {},
+            fields: fields,
+            domain: params.domain || [],
+        }).then((results) => {
+            this.data = _.map(results, (result) => {
+                return {
+                    id: result.id,
+                    latitude: result[params.latitudeField],
+                    longitude: result[params.longitudeField],
+                };
+            });
+        });
+    },
+});
 
 return MapModel;
 
--- awesome_map/static/src/js/map_view.js
+++ awesome_map/static/src/js/map_view.js
@@ -5,17 +5,28 @@ const AbstractView = require('web.AbstractView');
 const core = require('web.core');
 const viewRegistry = require('web.view_registry');
 
+const MapModel = require('awesome_map.MapModel');
 const MapRenderer = require('awesome_map.MapRenderer');
 
 const _lt = core._lt;
 
 const MapView = AbstractView.extend({
     config: _.extend({}, AbstractView.prototype.config, {
+        Model: MapModel,
         Renderer: MapRenderer,
     }),
     display_name: _lt('Map'),
     icon: 'fa-globe',
     viewType: 'awesome_map',
+
+    /**
+     * @override
+     */
+    init: function () {
+        this._super.apply(this, arguments);
+        this.loadParams.latitudeField = this.arch.attrs.latitude;
+        this.loadParams.longitudeField = this.arch.attrs.longitude;
+    },
 });
 
 viewRegistry.add('awesome_map', MapView);
