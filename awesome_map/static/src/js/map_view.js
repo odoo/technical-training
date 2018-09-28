b741f9da6939cf67bb4c5306e0adc40de0102a67
Aaron Bohy <aab@odoo.com>
2018-09-28 14:38:09 +0200

    solution Task 3.1

--- awesome_map/static/src/js/map_view.js
+++ awesome_map/static/src/js/map_view.js
@@ -2,9 +2,21 @@ odoo.define('awesome_map.MapView', function (require) {
 "use strict";
 
 const AbstractView = require('web.AbstractView');
+const core = require('web.core');
 const viewRegistry = require('web.view_registry');
 
-const MapView = AbstractView.extend({});
+const MapRenderer = require('awesome_map.MapRenderer');
+
+const _lt = core._lt;
+
+const MapView = AbstractView.extend({
+    config: _.extend({}, AbstractView.prototype.config, {
+        Renderer: MapRenderer,
+    }),
+    display_name: _lt('Map'),
+    icon: 'fa-globe',
+    viewType: 'awesome_map',
+});
 
 viewRegistry.add('awesome_map', MapView);
 
