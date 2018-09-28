88a7e08c150d67a75d43757218c433321c50fd9f
Aaron Bohy <aab@odoo.com>
2018-09-28 16:23:53 +0200

    solution Task 3.7

--- awesome_map/static/src/js/map_controller.js
+++ awesome_map/static/src/js/map_controller.js
@@ -2,11 +2,33 @@ odoo.define('awesome_map.MapController', function (require) {
 "use strict";
 
 var AbstractController = require('web.AbstractController');
+var core = require('web.core');
+
+var qweb = core.qweb;
 
 var MapController = AbstractController.extend({
     custom_events: _.extend({}, AbstractController.prototype.custom_events, {
         record_clicked: '_onRecordClicked',
     }),
+
+    //--------------------------------------------------------------------------
+    // Public
+    //--------------------------------------------------------------------------
+
+    /**
+     * @override
+     */
+    renderButtons: function ($node) {
+        var self = this;
+        this.$buttons = $(qweb.render("MapView.buttons", {widget: this}));
+        this.$buttons.on('click', 'button.o_map_zoom_in', function () {
+            self.renderer.zoomIn();
+        });
+        this.$buttons.on('click', 'button.o_map_zoom_out', function () {
+            self.renderer.zoomOut();
+        });
+        this.$buttons.appendTo($node);
+    },
 
     //--------------------------------------------------------------------------
     // Handlers
