3adff4faff3d3b2eb485870e1e626c1bca3ee8e5
Aaron Bohy <aab@odoo.com>
2018-09-28 11:11:55 +0200

    solution Task 2.7.2 (extra rpc)

--- awesome_tshirt/static/src/js/home_menu.js
+++ awesome_tshirt/static/src/js/home_menu.js
@@ -2,12 +2,24 @@
 odoo.define('awesome_tshirt.HomeMenu', function (require) {
 "use strict";
 
-var core = require('web.core');
 var HomeMenu = require('web_enterprise.HomeMenu');
 
-var _t = core._t;
-
 HomeMenu.include({
+    /**
+     * Overrides to load the custom message to display.
+     *
+     * @override
+     */
+    willStart: function () {
+        var self = this;
+        var superDef = this._super.apply(this, arguments);
+        var messageDef = this._rpc({
+            route: '/awesome_tshirt/bafienistalkingtoyou',
+        }).then(function (message) {
+            self.message = message;
+        });
+        return $.when(superDef, messageDef);
+    },
     //--------------------------------------------------------------------------
     // Private
     //--------------------------------------------------------------------------
@@ -20,7 +32,7 @@ HomeMenu.include({
         this._super.apply(this, arguments);
         var $message = $('<div>', {
             class: 'p-2 alert-warning o_custom_message',
-        }).text(_t("Bafien Ckinpaers is watching you!"));
+        }).text(this.message);
         $('<i class="fa fa-eye"></i><i class="fa fa-eye"></i>').appendTo($message);
         this.$('.o_custom_message').remove();
         this.$el.prepend($message);
