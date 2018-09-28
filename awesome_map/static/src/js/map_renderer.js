8694f332bb4421fe6fbb8735b011427d95238af3
Aaron Bohy <aab@odoo.com>
2018-09-28 14:38:09 +0200

    solution Task 3.1

--- awesome_map/static/src/js/map_renderer.js
+++ awesome_map/static/src/js/map_renderer.js
@@ -3,7 +3,19 @@ odoo.define('awesome_map.MapRenderer', function (require) {
 
 var AbstractRenderer = require('web.AbstractRenderer');
 
-var MapRenderer = AbstractRenderer.extend({});
+var MapRenderer = AbstractRenderer.extend({
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+
+    /**
+     * @override
+     */
+    _render: function () {
+        this.$el.text('Hello world');
+        return this._super.apply(this, arguments);
+    },
+});
 
 return MapRenderer;
 
--- awesome_map/static/src/js/map_view.js
+++ awesome_map/static/src/js/map_view.js
@@ -1,10 +1,21 @@
 odoo.define('awesome_map.MapView', function (require) {
 "use strict";
 
+var MapRenderer = require('awesome_map.MapRenderer');
+
 var AbstractView = require('web.AbstractView');
+var core = require('web.core');
 var viewRegistry = require('web.view_registry');
 
-var MapView = AbstractView.extend({});
+var _lt = core._lt;
+
+var MapView = AbstractView.extend({
+    config: _.extend({}, AbstractView.prototype.config, {
+        Renderer: MapRenderer,
+    }),
+    display_name: _lt('Map'),
+    icon: 'fa-globe',
+});
 
 viewRegistry.add('map', MapView);
 
