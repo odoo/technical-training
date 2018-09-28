0d320c497e722012cf07a3b514e31bda151ee448
Aaron Bohy <aab@odoo.com>
2018-09-28 10:55:26 +0200

    Solution Task 1.3A

--- awesome_tshirt/static/src/js/awesome_dashboard.js
+++ awesome_tshirt/static/src/js/awesome_dashboard.js
@@ -7,20 +7,70 @@ odoo.define('awesome_tshirt.dashboard', function (require) {
  * the orders and buttons to jump to specific views.
  */
 
-var MyCounter = require('awesome_tshirt.MyCounter');
-
 var AbstractAction = require('web.AbstractAction');
 var core = require('web.core');
 
+var _t = core._t;
+
 var Dashboard = AbstractAction.extend({
+    template: 'AwesomeDashboard',
+    events: {
+        'click .o_new_orders_btn': '_onOpenNewOrders',
+        'click .o_customers_btn': '_onOpenCustomers',
+        'click .o_cancelled_orders_btn': '_onOpenCancelledOrders',
+    },
+
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+
     /**
-     * @override
+     * Opens the list of orders created in the last 7 days.
+     *
+     * @private
+     * @param {Object} params
+     * @param {Array[]} [params.domain=[]] additional domain
+     * @param {string} params.name name of the action
+     * @returns {Promise} resolved when the action is executed
      */
-    start: function () {
-        var myCounter = new MyCounter(this);
-        var counterDef = myCounter.appendTo(this.$el);
-        var superDef = this._super.apply(this, arguments);
-        return $.when(counterDef, superDef);
+    _openLastWeekOrders: function (params) {
+       var aWeekAgo = moment().subtract(7,'d').locale('en').format('YYYY-MM-DD HH:mm:ss');
+        this.do_action({
+            name: params.name,
+            res_model: 'awesome_tshirt.order',
+            type: 'ir.actions.act_window',
+            views: [[false, 'list'], [false, 'form']],
+            domain: [['create_date', '>=', aWeekAgo]].concat(params.domain || []),
+        });
+    },
+
+    //--------------------------------------------------------------------------
+    // Handlers
+    //--------------------------------------------------------------------------
+
+    /**
+     * @private
+     */
+    _onOpenCancelledOrders: function () {
+        this._openLastWeekOrders({
+            name: _t('Cancelled Orders'),
+            domain: [['state', '=', 'cancelled']],
+        });
+    },
+    /**
+     * @private
+     */
+    _onOpenCustomers: function () {
+        this.do_action('base.action_partner_customer_form');
+    },
+    /**
+     * @private
+     */
+    _onOpenNewOrders: function () {
+        this._openLastWeekOrders({
+            name: _t('New Orders'),
+            domain: [['state', '=', 'new']],
+        });
     },
 });
 
