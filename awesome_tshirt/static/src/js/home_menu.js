b3390d4f306a06d764e4fac16d113da95377ae60
Aaron Bohy <aab@odoo.com>
2018-09-28 11:11:55 +0200

    solution Task 2.7.2 (extra rpc)

--- awesome_tshirt/static/src/js/home_menu.js
+++ awesome_tshirt/static/src/js/home_menu.js
@@ -8,6 +8,23 @@ const HomeMenu = require('web_enterprise.HomeMenu');
 const qweb = core.qweb;
 
 HomeMenu.include({
+    /**
+     * Overrides to load the custom message to display.
+     *
+     * @override
+     */
+    willStart: function () {
+        const messageProm = this._rpc({
+            route: '/awesome_tshirt/bafienistalkingtoyou',
+        }).then((message) => {
+            this.message = message;
+        });
+        return Promise.all([
+            this._super.apply(this, arguments),
+            messageProm
+        ]);
+    },
+
     //--------------------------------------------------------------------------
     // Private
     //--------------------------------------------------------------------------
@@ -19,7 +36,7 @@ HomeMenu.include({
     _render: function () {
         this._super.apply(this, arguments);
         this.$('.o_custom_message').remove();
-        this.$el.prepend(qweb.render('HomeMenu.Message'));
+        this.$el.prepend(qweb.render('HomeMenu.Message', {message: this.message}));
     },
 });
 
