2ad3649e114d89a328f7743c28a8406f6aec5ef9
jev-odoo <jev@odoo.com>
2018-12-17 12:08:56 +0100

    [IMP] technical-training: make v12 compatible
    Compatibility fixes for v12
    Bug fixes, ir.model.access.csv added, add some missings
    Made compliant with Odoo guidelines
    More unity between modules (End of module 1 is base of module 2, and so on.)

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -1,40 +1,19 @@
 # -*- coding: utf-8 -*-
-from datetime import timedelta
-from odoo import models, fields, api
-from odoo.exceptions import ValidationError
+from odoo import api, exceptions, fields, models
 
 
 class Course(models.Model):
     _name = 'openacademy.course'
+    _description = 'Course'
 
     name = fields.Char(name='Title', required=True)
     description = fields.Text()
-    responsible_id = fields.Many2one('res.users', ondelete='set null', string="Responsible", index=True)
+
+    responsible_id = fields.Many2one('res.users', ondelete='set null', string="Responsible")
     session_ids = fields.One2many('openacademy.session', 'course_id', string="Sessions")
+
     level = fields.Selection([(1, 'Easy'), (2, 'Medium'), (3, 'Hard')], string="Difficulty Level")
-    session_count = fields.Integer("Session Count", compute="_compute_session_count")
-
-    _sql_constraints = [
-       ('name_description_check', 'CHECK(name != description)',
-        "The title of the course should not be the description"),
-
-       ('name_unique', 'UNIQUE(name)',
-        "The course title must be unique"),
-    ]
-
-    @api.multi
-    def copy(self, default=None):
-        default = dict(default or {})
-
-        copied_count = self.search_count(
-            [('name', '=like', u"Copy of {}%".format(self.name))])
-        if not copied_count:
-            new_name = u"Copy of {}".format(self.name)
-        else:
-            new_name = u"Copy of {} ({})".format(self.name, copied_count)
-
-        default['name'] = new_name
-        return super(Course, self).copy(default)
+    session_count = fields.Integer(compute="_compute_session_count")
 
     @api.depends('session_ids')
     def _compute_session_count(self):
@@ -44,33 +23,28 @@ class Course(models.Model):
 
 class Session(models.Model):
     _name = 'openacademy.session'
-    _order = 'name'
+    _description = 'Session'
 
     name = fields.Char(required=True)
-    start_date = fields.Date(default=lambda self : fields.Date.today())
-    #end_date = fields.Date(default=lambda self : fields.Date.today())
-    end_date = fields.Date(string='End date', store=True, compute='_get_end_date', inverse='_set_end_date')
+    description = fields.Html()
     active = fields.Boolean(default=True)
-    duration = fields.Float(digits=(6, 2), help="Duration in days", default=1)
-    seats = fields.Integer(string="Number of seats")
-    instructor_id = fields.Many2one('res.partner', string="Instructor") #No ondelete = set null
-    course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
-    attendee_ids = fields.Many2many('res.partner', string="Attendees")
-    taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
+    state = fields.Selection([('draft', "Draft"), ('confirmed', "Confirmed"), ('done', "Done")], default='draft')
     level = fields.Selection(related='course_id.level', readonly=True)
     responsible_id = fields.Many2one(related='course_id.responsible_id', readonly=True, store=True)
 
-    percentage_per_day = fields.Integer("%", default=100)
-    attendees_count = fields.Integer(string="Attendees count", compute='_get_attendees_count', store=True)
+    start_date = fields.Date(default=fields.Date.context_today)
+    end_date = fields.Date(default=fields.Date.today)
+    duration = fields.Float(digits=(6, 2), help="Duration in days", default=1)
 
-    def _warning(self, title, message):
-        return {'warning': {
-            'title': title,
-            'message': message,
-        }}
+    instructor_id = fields.Many2one('res.partner', string="Instructor")
+    course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
+    attendee_ids = fields.Many2many('res.partner', string="Attendees")
+    attendees_count = fields.Integer(compute='_get_attendees_count', store=True)
+    seats = fields.Integer()
+    taken_seats = fields.Float(compute='_compute_taken_seats', store=True)
 
     @api.depends('seats', 'attendee_ids')
-    def _taken_seats(self):
+    def _compute_taken_seats(self):
         for session in self:
             if not session.seats:
                 session.taken_seats = 0.0
@@ -82,36 +56,14 @@ class Session(models.Model):
         for session in self:
             session.attendees_count = len(session.attendee_ids)
 
-    @api.onchange('seats', 'attendee_ids')
-    def _verify_valid_seats(self):
-        if self.seats < 0:
-            return self._warning("Incorrect 'seats' value",  "The number of available seats may not be negative")
-        if self.seats < len(self.attendee_ids):
-            return self._warning("Too many attendees", "Increase seats or remove excess attendees")
-
-    @api.constrains('instructor_id', 'attendee_ids')
-    def _check_instructor_not_in_attendees(self):
-        for session in self:
-            if session.instructor_id and session.instructor_id in session.attendee_ids:
-                raise ValidationError("A session's instructor can't be an attendee")
-
-    @api.depends('start_date', 'duration')
-    def _get_end_date(self):
-        for session in self:
-            if not (session.start_date and session.duration):
-                session.end_date = session.start_date
-            else:
-                # Add duration to start_date, but: Monday + 5 days = Saturday,
-                # so subtract one second to get on Friday instead
-                start = fields.Datetime.from_string(session.start_date)
-                duration = timedelta(days=session.duration, seconds=-1)
-                session.end_date = start + duration
-
-    def _set_end_date(self):
-        for session in self:
-            if session.start_date and session.end_date:
-                # Compute the difference between dates, but: Friday - Monday = 4
-                # days, so add one day to get 5 days instead
-                start_date = fields.Datetime.from_string(session.start_date)
-                end_date = fields.Datetime.from_string(session.end_date)
-                session.duration = (end_date - start_date).days + 1
+    @api.onchange('start_date', 'end_date')
+    def _compute_duration(self):
+        if not (self.start_date and self.end_date):
+            return
+        if self.end_date < self.start_date:
+            return {'warning': {
+                'title':   "Incorrect date value",
+                'message': "End date is earlier then start date",
+            }}
+        delta = fields.Date.from_string(self.end_date) - fields.Date.from_string(self.start_date)
+        self.duration = delta.days + 1
