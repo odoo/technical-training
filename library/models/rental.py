e9eded23263626688f0fffc533e9a60538aa5459
jev-odoo <jev@odoo.com>
2018-12-17 11:53:38 +0100

    [IMP] technical-training: make v12 compatible
    Compatibility fixes for v12
    Bug fixes, ir.model.access.csv added, add some missings
    Made compliant with Odoo guidelines
    More unity between modules (End of module 1 is base of module 2, and so on.)

--- library/models/rental.py
+++ library/models/rental.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api, exceptions, _
+from odoo import fields, models
+
 
 class Rentals(models.Model):
     _name = 'library.rental'
@@ -7,5 +8,6 @@ class Rentals(models.Model):
 
     customer_id = fields.Many2one('library.partner', string='Customer')
     book_id = fields.Many2one('library.book', string='Book')
-    rental_date = fields.Date(string='Rental date')
-    return_date = fields.Date(string='Return date')
+
+    rental_date = fields.Date()
+    return_date = fields.Date()
---
+++ openacademy/models/course.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, models
+
+
+class Course(models.Model):
+    _name = 'openacademy.course'
+    _description = 'Course'
+
+    name = fields.Char(name='Title', required=True)
+    description = fields.Text()
+
+    responsible_id = fields.Many2one('openacademy.partner', string="Responsible")
+    session_ids = fields.One2many('openacademy.session', 'course_id', string="Sessions")
+
+    level = fields.Selection([(1, 'Easy'), (2, 'Medium'), (3, 'Hard')], string="Difficulty Level")
+
+
+class Session(models.Model):
+    _name = 'openacademy.session'
+    _description = 'Session'
+
+    name = fields.Char(required=True)
+    active = fields.Boolean(default=True)
+    state = fields.Selection([('draft', "Draft"), ('confirmed', "Confirmed"), ('done', "Done")], default='draft')
+
+    start_date = fields.Date(default=fields.Date.context_today)
+    duration = fields.Float(digits=(6, 2), help="Duration in days", default=1)
+
+    instructor_id = fields.Many2one('openacademy.partner', string="Instructor")
+    course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
+    attendee_ids = fields.Many2many('openacademy.partner', string="Attendees")
