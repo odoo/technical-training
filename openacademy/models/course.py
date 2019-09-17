acb1488da9c4418850d959ea32872e4a068ef61b
Stanislas Sobieski <sts@odoo.com>
2019-09-17 14:57:21 +0200

    [MIG] force key in select field as string a,nd change some fields

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -12,7 +12,7 @@ class Course(models.Model):
     responsible_id = fields.Many2one('res.users', ondelete='set null', string="Responsible")
     session_ids = fields.One2many('openacademy.session', 'course_id', string="Sessions")
 
-    level = fields.Selection([(1, 'Easy'), (2, 'Medium'), (3, 'Hard')], string="Difficulty Level")
+    level = fields.Selection([('1', 'Easy'), ('2', 'Medium'), ('3', 'Hard')], string="Difficulty Level")
     session_count = fields.Integer(compute="_compute_session_count")
 
     @api.depends('session_ids')
