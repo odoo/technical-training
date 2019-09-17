e73ff016508537027474a27dfe6aa26a395db0e2
Stanislas Sobieski <sts@odoo.com>
2019-09-17 14:05:36 +0200

    [MIG] Force string on selection key

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -12,7 +12,7 @@ class Course(models.Model):
     responsible_id = fields.Many2one('openacademy.partner', string="Responsible")
     session_ids = fields.One2many('openacademy.session', 'course_id', string="Sessions")
 
-    level = fields.Selection([(1, 'Easy'), (2, 'Medium'), (3, 'Hard')], string="Difficulty Level")
+    level = fields.Selection([('1', 'Easy'), ('2', 'Medium'), ('3', 'Hard')], string="Difficulty Level")
 
 
 class Session(models.Model):
