c2a9c64dd195bb5966706fd847cfbf8e1db67fb2
Sofiane Azarkan <sofiane.azarkan@gmail.com>
2022-12-07 11:51:59 +0100

    ajout champ max_amount

--- estate/models/res_groups.py
+++ estate/models/res_groups.py
@@ -2,3 +2,4 @@ from odoo import fields, models
 
 class ResGroups(models.Model):
     _inherit = 'res.groups'
+    max_amount = fields.Float(string="Training Max Price (Approval)") #Création d'un nouveau champ dans la DB de type Float
