70371a2fc01b7a6bc1947d8153bba4022948c10a
Sofiane Azarkan <sofiane.azarkan@gmail.com>
2022-12-13 20:44:01 +0100

    Ajout champ training_date dans sale_order_line

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -8,6 +8,5 @@
     'version': '16.0.0.1',
     'license': 'LGPL-3',
     'depends': ['base', 'sale_management', 'calendar', 'hr'],
-    'data': [
-    ]
+    'data': ['views/res_groups.xml', 'views/res_partner.xml', 'views/sale_order.xml']
 }
--- estate/models/sale_order_line.py
+++ estate/models/sale_order_line.py
@@ -2,3 +2,4 @@ from odoo import fields, models, Command
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
+    training_date = fields.Char(string="Training date") #attention revoir le type
