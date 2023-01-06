879e857c17c0340b2da13785eeea29d808ec536c
Akalai21 <ousama.akalaihayyoun@helb-prigogine.be>
2023-01-06 22:58:46 +0100

    Add fields employee

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -3,3 +3,18 @@ from odoo.exceptions import ValidationError
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
+    employee_id = fields.Many2one('res.partner', string="Employee")
+
+
+@api.multi
+def action_confirm(self):
+    # Create the event in the calendar of the selected employee
+    self.ensure_one()
+    event_vals = {
+        'name': 'Training',
+        'start': self.order_line.training_date,
+        'stop': self.order_line.training_date,
+        'partner_ids': [(6, 0, [self.employee_id.id])],
+    }
+    event = self.env['calendar.event'].create(event_vals)
+    return super(SaleOrder, self).action_confirm()    
