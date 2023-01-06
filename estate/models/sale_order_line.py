38bab65eac80eff03864bfb78f217b846e360b91
Akalai21 <ousama.akalaihayyoun@helb-prigogine.be>
2023-01-06 23:25:25 +0100

    test_v1

--- estate/models/sale_order_line.py
+++ estate/models/sale_order_line.py
@@ -3,3 +3,7 @@ from odoo import fields, models, Command
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
     training_date = fields.Char(string="Training Date")
+    
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+    employee_id = fields.Many2one('res.partner', string="Employee")
