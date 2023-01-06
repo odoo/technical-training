30e5c93d79016fb1bcc6ee13974fbac6dd13b3d7
Akalai21 <ousama.akalaihayyoun@helb-prigogine.be>
2023-01-07 00:09:10 +0100

    first question

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -5,6 +5,19 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
 
+@api.multi
+def action_confirm(self):
+    res = super(SaleOrder, self).action_confirm()
+    for order in self:
+        for line in order.order_line:
+            if line.training_dates and line.employee_id:
+                self.env['calendar.event'].create({
+                    'name': 'Training session',
+                    'start_date': line.training_dates,
+                    'stop_date': line.training_dates + timedelta(hours=1),
+                    'partner_ids': [(4, line.employee_id.id)],
+                })
+    return res
  
 
     
