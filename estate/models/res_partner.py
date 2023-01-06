8f07ff1ebcf67d906777df61c10d8ce3d5b0c964
mbensafia <92778589+mbensafia@users.noreply.github.com>
2023-01-07 00:55:00 +0100

    add sale order limit

--- estate/models/res_partner.py
+++ estate/models/res_partner.py
@@ -2,3 +2,4 @@ from odoo import fields, models
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
+     max_amount = fields.Float(string="Maximum allowed amount")
--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -4,6 +4,14 @@ from odoo.exceptions import ValidationError
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
+     @api.constrains('partner_id', 'order_line')
+    def check_max_amount(self):
+      for order in self:
+        if order.partner_id.max_amount > 0:
+          total_amount = sum(line.price_total for line in order.order_line)
+          if total_amount > order.partner_id.max_amount:
+            raise ValidationError(_("The total amount of this sale order exceeds the maximum allowed for the partner."))
+
 
 
 def action_confirm(self):
@@ -18,6 +26,8 @@ def action_confirm(self):
                     'partner_ids': [(4, line.employee_id.id)],
                 })
     return res
+
+
  
 
     
