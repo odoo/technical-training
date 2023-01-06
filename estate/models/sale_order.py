f2616715da02c0ae7fe3eb00b60b87fd21bf9de2
Akalai21 <ousama.akalaihayyoun@helb-prigogine.be>
2023-01-06 23:03:46 +0100

    Test creation employee

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -6,15 +6,6 @@ class SaleOrder(models.Model):
     employee_id = fields.Many2one('res.partner', string="Employee")
 
 
-@api.multi
-def action_confirm(self):
-    # Create the event in the calendar of the selected employee
-    self.ensure_one()
-    event_vals = {
-        'name': 'Training',
-        'start': self.order_line.training_date,
-        'stop': self.order_line.training_date,
-        'partner_ids': [(6, 0, [self.employee_id.id])],
-    }
-    event = self.env['calendar.event'].create(event_vals)
-    return super(SaleOrder, self).action_confirm()    
+ 
+
+    
