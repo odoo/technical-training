0fb6a0ff70b87e0c1363e8920bfd31e8747aae25
Akalai21 <ousama.akalaihayyoun@helb-prigogine.be>
2023-01-06 23:33:47 +0100

    test_v5

--- estate/models/sale_order_line.py
+++ estate/models/sale_order_line.py
@@ -4,3 +4,6 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
     training_date = fields.Char(string="Training Date")
     
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+    employee_id = fields.Many2one('res.partner', string="Employee")
