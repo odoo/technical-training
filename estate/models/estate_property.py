d47126ade584628700f2b6c3b8c558b93901a63e
_goEko_ <goeko@goeko.de>
2023-11-06 12:21:02 +0100

    CAP 6 : Views / add active and status

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -22,8 +22,8 @@ class EstateProperty(models.Model):
         string='Type',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West') ],
         help="")
-    active = fields.Boolean()
     state = fields.Selection(
         string='State',
         selection=[('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('canceled', 'Canceled') ],
         help="")
+    active = fields.Boolean()
