1fae4715aa5f54e3798c474b518fe97b0cd8378f
_goEko_ <goeko@goeko.de>
2023-11-06 15:35:58 +0100

    Chapter 8

---
+++ estate/models/estate_property_type.py
@@ -0,0 +1,29 @@
+from dateutil.relativedelta import relativedelta
+from odoo import fields, models
+
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Estate Property"
+
+    name = fields.Char()
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(default=lambda self: fields.Date.today() + relativedelta(months=+3))
+    expected_price = fields.Float()
+    selling_price = fields.Float()
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        string='Type',
+        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West') ],
+        help="")
+    state = fields.Selection(
+        string='State',
+        selection=[('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('canceled', 'Canceled') ],
+        help="")
+    active = fields.Boolean()
