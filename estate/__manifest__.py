6eba645be1dade7e73534167234dc742f09fb889
_goEko_ <goeko@goeko.de>
2023-11-06 11:23:53 +0100

    CAP 6 : Views

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,7 +4,8 @@
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     "data": [
-        "security/ir.model.access.csv"
+        "security/ir.model.access.csv",
+        "views/estate_property.xml"
     ],
     "installable": True,
     'license': 'LGPL-3',
