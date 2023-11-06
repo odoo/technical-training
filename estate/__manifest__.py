c687ef10fa49fa6d661dff76e72e0e600459061c
_goEko_ <goeko@goeko.de>
2023-11-06 11:31:15 +0100

    CAP 6 : Views

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,7 +5,7 @@
     "depends": ["base"],  # dependencies
     "data": [
         "security/ir.model.access.csv",
-        "views/estate_property.xml"
+        "views/estate_property_views.xml"
     ],
     "installable": True,
     'license': 'LGPL-3',
