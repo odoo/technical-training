80c089b8599825edfe64bd2b0010b34d26807d1c
_goEko_ <goeko@goeko.de>
2023-11-06 17:08:03 +0100

    ... Chapter 8 : One2many : offer

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -8,6 +8,7 @@
         "views/estate_property_views.xml",
         "views/estate_property_type_views.xml",
         "views/estate_property_tag_views.xml",
+        "views/estate_property_offer_views.xml",
         "views/estate_menus.xml"
     ],
     "installable": True,
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,8 +1,14 @@
 from odoo import fields, models
 
 
-class EstatePropertyType(models.Model):
-    _name = "estate.property.type"
-    _description = "Estate Property Type"
+class EstatePropertyOffer(models.Model):
+    _name = "estate.property.offer"
+    _description = "Estate Property Offer"
 
-    name = fields.Char()
+    price = fields.Float()
+    status = fields.Selection(
+        string='Type',
+        selection=[('accepted', 'Accepted'), ('refused', 'Refused') ],
+        help="")
+    partner_id = fields.Many2one("res.partner", string="Partner")
+    property_id = fields.Many2one("estate.property", string="Property")
