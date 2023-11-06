cb105e4e9cfb289004468652394c1d55331901a8
_goEko_ <goeko@goeko.de>
2023-11-06 11:01:17 +0100

    CAP 5 : Access Rights

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,7 +4,7 @@
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     "data": [
-
+        "security/ir.model.access.csv"
     ],
     "installable": True,
     'license': 'LGPL-3',
