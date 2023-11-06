afa76fd95be500b2d732da03cc7b6baa8f64b7ce
_goEko_ <goeko@goeko.de>
2023-11-06 16:32:44 +0100

    ... Chapter 8 : many2many : tags

--- estate/models/estate_property_tag.py
+++ estate/models/estate_property_tag.py
@@ -1,7 +1,7 @@
 from odoo import fields, models
 
 
-class EstatePropertyType(models.Model):
+class EstatePropertyTag(models.Model):
     _name = "estate.property.tag"
     _description = "Estate Property Tag"
 
