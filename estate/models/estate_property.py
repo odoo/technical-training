ab3d96a09487a85d593f2dcd7320d2c1004f9e45
_goEko_ <goeko@goeko.de>
2023-11-07 11:31:46 +0100

    ... Chapter 9 : Onchanges : garden

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -45,7 +45,7 @@ class EstateProperty(models.Model):
     def _onchange_garden(self):
         if self.garden == 1:
             self.garden_area = 10
-            self.garden_orientation = "North"
+            self.garden_orientation = "north"
         else:
             self.garden_area = 0
             self.garden_orientation = ""
