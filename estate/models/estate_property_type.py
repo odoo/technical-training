3ce2b9b822198d85f461490370be0adcb2ab8283
_goEko_ <goeko@goeko.de>
2023-11-07 15:30:20 +0100

    Chapter 12: Exercise : Add a stat button to property type.

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -6,7 +6,7 @@ class EstatePropertyType(models.Model):
     _description = "Estate Property Type"
 
     name = fields.Char()
-    offer_ids = fields.One2many("estate.property.type", "property_type_id", string="Offers")
+    offer_ids = fields.One2many("estate.property.offer", "property_type_id", string="Offers")
     offer_count = fields.Integer(string='Offer Count', compute='_compute_offer_count')
 
     _sql_constraints = [
