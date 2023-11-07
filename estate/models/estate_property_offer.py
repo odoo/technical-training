5bc4d6c0da6838583b648c0f68522b42311ae16e
_goEko_ <goeko@goeko.de>
2023-11-07 10:56:47 +0100

    ... Chapter 8 : One2many : offer : Inverse Function

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -15,7 +15,7 @@ class EstatePropertyOffer(models.Model):
     partner_id = fields.Many2one("res.partner", string="Partner")
     property_id = fields.Many2one("estate.property", string="Property")
     validity = fields.Integer(default=7)
-    date_deadline = fields.Date(compute="_compute_date_deadline", inverse="_inverse_date_deadline1")
+    date_deadline = fields.Date(compute="_compute_date_deadline", inverse="_inverse_date_deadline", store=True)
 
     @api.depends("validity")
     def _compute_date_deadline(self):
@@ -25,7 +25,7 @@ class EstatePropertyOffer(models.Model):
             else:
                 record.date_deadline = fields.Date.today() + relativedelta(days=record.validity)
 
-    def _inverse_date_deadline1(self):
+    def _inverse_date_deadline(self):
         for record in self:
             if record.create_date:
                 record.validity = (record.date_deadline - record.create_date).days
