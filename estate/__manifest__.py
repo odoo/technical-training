688cf00b7964a8a60c2b493a4c1f79186d24c9dc
_goEko_ <goeko@goeko.de>
2023-11-07 13:22:28 +0100

    setup app version to 1.0.0 and odoo version to 17.0

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Real Estate",  # The name that will appear in the App list
-    "version": "16.0",  # Version
+    "version": "17.0.1.0.0",  # Version
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     "data": [
