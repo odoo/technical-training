246fb6cbe74a52209d147b468a4b715b3a7aa99f
_goEko_ <goeko@goeko.de>
2023-11-07 09:46:49 +0100

    ... Chapter 8 : One2many : offer : Inverse Function

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,3 +1,4 @@
+from dateutil.relativedelta import relativedelta
 from odoo import fields, models
 
 
@@ -13,3 +14,13 @@ class EstatePropertyOffer(models.Model):
         help="")
     partner_id = fields.Many2one("res.partner", string="Partner")
     property_id = fields.Many2one("estate.property", string="Property")
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(compute="_compute_date_deadline")
+
+    @api.depends("validity")
+    def _compute_date_deadline(self):
+        for record in self:
+            if record.create_date:
+                record.date_deadline = record.create_date + relativedelta(months=+(record.validity))
+            else:
+                record.date_deadline = fields.Date.today() + relativedelta(months=+(record.validity))
