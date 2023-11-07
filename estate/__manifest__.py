3ee2df9ee2e2e98462466960f3379dd68e3d6532
_goEko_ <goeko@goeko.de>
2023-11-07 15:37:16 +0100

    Chapter 12: Exercise : Add a stat button to property type.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,6 +5,7 @@
     "depends": ["base"],  # dependencies
     "data": [
         "security/ir.model.access.csv",
+        "views/estate_actions.xml",
         "views/estate_property_views.xml",
         "views/estate_property_type_views.xml",
         "views/estate_property_tag_views.xml",
